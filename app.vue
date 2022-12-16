86c5315426aa2469cea6e64c83f104974ae49b87
Rodrigo <rodrigohths@gmail.com>
2022-12-16 16:50:56 -0300

    fix: typo

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
   <div>
-    <MaterialCard tittle="teste"
-      description="
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis incidunt quia ipsam minima, natus officia reprehenderit mollitia quam atque obcaecati expedita aliquid amet debitis enim laudantium ea, pariatur tenetur?"
-      color="he4rt"></MaterialCard>
-    <MaterialCard tittle="teste"
-      description="
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat corporis incidunt quia ipsam minima, natus officia reprehenderit mollitia quam atque obcaecati expedita aliquid amet debitis enim laudantium ea, pariatur tenetur?">
-    </MaterialCard>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
