a2c59788a23c60d5b40ddfa25ddd291ce8314a50
Giovane Cardoso <novout@hotmail.com>
2022-12-16 19:40:00 -0300

    feat(dashboard): set activity items tracking

--- components/dashboard/DashboardTeamActivity.vue
+++ components/dashboard/DashboardTeamActivity.vue
@@ -1,7 +1,54 @@
 <template>
-  <div class="flex flex-col w-full bg-h-dark-one rounded px-5 py-10 mb-12">
-    <h2 class="font-inter pb-5 border-b border-h-divider font-bold text-h-light text-lg">Registro de Atividades</h2>
-    <div class="h-140 w-full mt-10 overflow-y-auto">wip</div>
-    <div class="flex justify-center font-dm font-bold text-h-second-purple">ver mais</div>
+  <div class="flex flex-col w-full bg-h-dark-one rounded px-5 pt-10 pb-12 mb-12 overflow-y-auto h-200">
+    <h2 class="font-inter pb-3 border-b border-h-divider font-bold text-h-light text-lg">Registro de Atividades</h2>
+    <div class="flex flex-col w-full mt-10 gap-10">
+      <DashboardTeamActivityItem
+        :title="item.title"
+        :description="item.description"
+        :color="item.color"
+        :time="item.time"
+        :username="item.username"
+        :username_image_url="item.username_image_url"
+        :starAmount="item.starAmount"
+        v-for="(item, index) in list"
+        :key="index"
+      />
+    </div>
+    <div class="flex justify-center font-dm font-bold text-h-second-purple mt-12 cursor-pointer tracking-wide">ver mais</div>
   </div>
 </template>
+
+<script setup lang="ts">
+  const list = useState('list', () => [
+    {
+      title: '[UI/UX] Criar Design System He4rt Devs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis in massa in malesuada. Aenean in quam vitae ex tincidunt imperdiet non vitae sapien.',
+      color: 'done' as any,
+      time: 3,
+      username: 'NeexTuRHe4rt',
+      username_image_url: 'https://i.imgur.com/vlU6ZAZ.jpg',
+      starAmount: 4,
+    },
+    {
+      title: '[UI/UX] Criar Design System He4rt Devs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis in massa in malesuada. Aenean in quam vitae ex tincidunt imperdiet non vitae sapien.',
+      color: 'doing' as any,
+      time: 3,
+      username: 'NeexTuRHe4rt',
+      username_image_url: 'https://i.imgur.com/vlU6ZAZ.jpg',
+      starAmount: 4,
+    },
+    {
+      title: '[UI/UX] Criar Design System He4rt Devs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed venenatis in massa in malesuada. Aenean in quam vitae ex tincidunt imperdiet non vitae sapien.',
+      color: 'unavailable' as any,
+      time: 3,
+      username: 'NeexTuRHe4rt',
+      username_image_url: 'https://i.imgur.com/vlU6ZAZ.jpg',
+      starAmount: 4,
+    },
+  ])
+</script>
--- components/dashboard/DashboardTeamActivityItem.vue
+++ components/dashboard/DashboardTeamActivityItem.vue
@@ -5,30 +5,30 @@
       color === 'unavailable' ? 'border-l-solid border-red-600 ' : '',
       color === 'doing' ? 'border-l-solid border-yellow-400 ' : '',
     ]"
-    class="w-full h-42 bg-h-dark-two rounded box-border overflow-y-auto text-justify flex items-center"
+    class="w-full bg-h-dark-two rounded box-border overflow-y-auto text-justify flex items-center"
   >
-    <div class="grow p-5">
-      <div class="flex items-center gap-3">
+    <div class="p-5">
+      <div class="flex items-center gap-5">
         <div class="flex items-center bg-h-divider rounded w-5 h-5 justify-center">
-          <IconCheck v-if="color === 'done'" class="w-3 h-3 text-green-500" />
-          <IconClose v-else-if="color === 'doing'" class="w-3 h-3 text-yellow-400" />
-          <IconExclamation v-else-if="color === 'unavailable'" class="w-3 h-3 text-red-600" />
+          <IconCheck v-if="color === 'done'" class="w-4 h-4 text-green-500" />
+          <IconExclamation v-else-if="color === 'doing'" class="w-4 h-4 text-yellow-400" />
+          <IconClose v-else-if="color === 'unavailable'" class="w-4 h-4 text-red-600" />
         </div>
         <div class="flex items-center bg-h-divider rounded w-5 h-5 justify-center">
           <IconFireTwo class="w-3 h-3 text-h-gray" />
         </div>
 
-        <h1 class="text-h-light">{{ title }}</h1>
+        <h1 class="text-h-light text-lg">{{ title }}</h1>
       </div>
-      <div class="flex items-center justify-between py-4">
-        <p class="text-h-gray">{{ description }}</p>
+      <div class="flex items-center justify-between py-5">
+        <p class="text-h-gray w-3/4 leading-7 font-inter font-semibold">{{ description }}</p>
       </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center justify-between text-h-light">
-          <img class="rounded-full w-7 h-7 border-2 border-h-gray" src="https://i.imgur.com/vlU6ZAZ.jpg" />
+      <div class="flex items-center">
+        <div class="flex items-center justify-between text-h-light pr-15">
+          <img class="rounded-full w-7 h-7 border-2 border-h-gray mr-2" :src="username_image_url" />
           <p>{{ username }}</p>
         </div>
-        <div class="flex items-center justify-between text-h-gray">
+        <div class="flex items-center justify-between text-h-gray mr-15">
           <IconClock />
           <p>{{ time }} hrs</p>
         </div>
@@ -37,7 +37,7 @@
           <p v-else-if="color === 'doing'" class="text-yellow-400"><IconClose class="w-4 h-4" /> Em andamento</p>
           <p v-else-if="color === 'unavailable'" class="text-red-600"><IconExclamation class="w-4 h-4" />Indisponível</p>
         </div>
-        <div class="flex items-center gap-2">
+        <div class="flex items-center gap-2 ml-30">
           <IconStar v-if="typeof starAmount === 'number'" class="w-5 h-5 text-yellow-400" />
           <div v-else="!starAmount" class="text-h-gray"><IconStar class="w-5 h-5 text-h-gray" /> Avaliar</div>
           <p class="text-yellow-400">{{ starAmount }}</p>
@@ -46,10 +46,7 @@
         <p></p>
       </div>
     </div>
-
-    <div class="p-5">
-      <button class="font-semibold bg-h-purple py-4 px-6 rounded border-none text-h-light">Abrir Task</button>
-    </div>
+    <button class="font-semibold bg-h-purple py-4 px-6 rounded border-none text-h-light">Abrir Task</button>
   </div>
 </template>
 
@@ -60,6 +57,7 @@
     color?: 'done' | 'doing' | 'unavailable'
     time: number
     username: string
+    username_image_url: string
     starAmount?: number | ''
   }>()
 </script>
